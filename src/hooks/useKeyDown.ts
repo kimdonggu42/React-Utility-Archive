c57dadc2e4a0fe0691d13c3b96fa4588e2605768
kimdonggu42 <kdk3280@undermilli.com>
2024-08-15 23:27:27 +0900

    useKeyDown 커스텀 훅 추가

---
+++ src/hooks/useKeyDown.ts
@@ -0,0 +1,23 @@
+// 특정 키보드 입력 발생 시 콜백으로 전달받은 함수를 실행하는 커스텀 훅
+
+import { useEffect } from 'react';
+
+export const useKeyDown = (keys: string[], callback: () => void) => {
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      const wasAnyKeyPresses = keys.some((key) => e.key === key);
+
+      if (wasAnyKeyPresses) callback();
+    };
+    window.addEventListener('keydown', listener);
+
+    return () => {
+      window.removeEventListener('keydown', listener);
+    };
+  }, [keys, callback]);
+};
+
+// 사용법
+// useKeyDown(['Escape', ...], () => {
+//   action();
+// });

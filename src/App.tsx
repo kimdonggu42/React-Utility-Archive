0a2771814e50ed95de27ea04d5546c7ce3e8683e
kimdonggu42 <kdk3280@undermilli.com>
2024-08-17 22:20:01 +0900

    useInfiniteScroll 커스텀 훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,7 @@ import axios from 'axios';
 
 import { useEffect } from 'react';
 
-import { useThrottle } from './hooks/useThrottle';
-
 export default function App() {
-  const throttle = useThrottle();
-
-  const test = () => {
-    throttle(() => console.log('실행'), 2000);
-  };
-
   useEffect(() => {
     const getData = async () => {
       try {
@@ -23,5 +15,5 @@ export default function App() {
     getData();
   }, []);
 
-  return <button onClick={test}>클릭</button>;
+  return <></>;
 }
---
+++ src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,35 @@
+// 참고: MDN IntersectionObserver(https://developer.mozilla.org/ko/docs/Web/API/IntersectionObserver)
+import { useEffect, useRef } from 'react';
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1,
+};
+
+export const useInfiniteScroll = (callback: () => void) => {
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) callback();
+      });
+    };
+    const observer = new IntersectionObserver(handleIntersect, options);
+    const curTargetRef = targetRef.current;
+
+    if (curTargetRef) observer.observe(curTargetRef);
+
+    return () => {
+      if (curTargetRef) observer.unobserve(curTargetRef);
+    };
+  }, [callback]);
+
+  return targetRef;
+};
+
+// 사용법
+// const topRef = useInfiniteScroll(() => {
+//   getNextPageMessages();
+// });

a4178e55a9c1a658a66dc8270e6f7d04b5e2e4bd
kimdonggu42 <kdk3280@undermilli.com>
2024-11-12 20:42:18 +0900

    feat: debounce 페이지에 샘플 추가

--- src/routes/DebouncePage.tsx
+++ src/routes/DebouncePage.tsx
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+import { useDebounce } from '@/hooks/useDebounce';
+
 export default function DebouncePage() {
-  return <div>디바운스 페이지</div>;
+  const [text, setText] = useState<string>('');
+
+  const debounce = useDebounce();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+    debounce(() => {
+      alert('마지막으로 입력한지 2초가 지났습니다!');
+    }, 2000);
+  };
+
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <input className='border border-black' value={text} onChange={handleInputChange} />
+    </div>
+  );
 }

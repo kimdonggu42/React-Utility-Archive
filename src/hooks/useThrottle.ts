d74483a043b9ab891aa8f52f50690a34a71aa702
kimdonggu42 <kdk3280@undermilli.com>
2024-11-13 21:30:44 +0900

    feat: throttle 샘플 추가

--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,5 +1,7 @@
 // 짧은 시간 간격으로 이벤트가 연속해서 발생하더라도
-// 일정 시간 간격으로 이벤트 핸들러가 최대 한 번만 호출되도록 하는 커스텀 훅
+// 일정 시간 간격으로 이벤트 핸들러가 최대 한 번만 호출되도록 하는 커스텀 훅(일정 시간이 지나기 전에 재호출 방지)
+// ex: 버튼을 클릭할 때 마다 비용이 많이 드는 계산이 실행되는 것을 방지하기 위해
+// 버튼을 연타해도 일정 간격으로 이벤트가 실행되도록 함
 import { useRef } from 'react';
 
 export const useThrottle = () => {
@@ -8,8 +10,7 @@ export const useThrottle = () => {
 
   return (callback: () => void, delay: number) => {
     // 이후 throttle이 트리거 되면 트리거 될 당시의 시간과 lastRun의 차이를 계산해서
-    // 그 차이가 delay 이상이라면 lastRun이 기록된 이후로 delay 만큼의 시간이 지났다는 뜻이므로
-    // callback을 호출 함
+    // 그 차이가 delay 이상이라면 lastRun이 기록된 이후로 delay 만큼의 시간이 지났다는 뜻이므로 callback을 호출 함
     const timeElapsed = Date.now() - lastRun.current;
 
     if (timeElapsed >= delay) {
--- src/routes/ThrottlePage.tsx
+++ src/routes/ThrottlePage.tsx
@@ -1,3 +1,34 @@
+import { useState } from 'react';
+import { useThrottle } from '@/hooks/useThrottle';
+
 export default function ThrottlePage() {
-  return <div>쓰로틀 페이지</div>;
+  const [numbers, setNumbers] = useState<number[]>([0, 0, 0, 0]);
+
+  const throttle = useThrottle();
+
+  const resetNumbers = (numbers: number[]) => {
+    const newNumbers: number[] = [];
+    numbers.forEach((number: number) => newNumbers.push(Math.floor(number + Math.random() * 10)));
+    setNumbers(newNumbers);
+  };
+
+  const handleResetNumbers = () => {
+    console.log('버튼 클릭');
+    throttle(() => resetNumbers(numbers), 2000);
+  };
+
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <div className='flex flex-col gap-y-5'>
+        <button className='bg-blue-300 p-1' onClick={handleResetNumbers}>
+          재설정
+        </button>
+        <ul className='flex gap-x-10'>
+          {numbers.map((number, index) => (
+            <li key={index}>{number}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }

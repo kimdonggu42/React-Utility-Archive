ae51b8371ce9bdfe72ff6364f50a9c8da40f13fe
kimdonggu42 <kdk3280@undermilli.com>
2024-11-21 22:04:13 +0900

    useThrottle 훅에서 최초 트리거 시에는 throttle이 적용되지 않도록 로직 수정

--- src/throttle/useThrottle.ts
+++ src/throttle/useThrottle.ts
@@ -6,16 +6,22 @@ import { useRef } from 'react';
 
 export const useThrottle = () => {
   // lastRun은 최초 const throttle = useThrottle(); 이렇게 선언되었을 때의 시간을 기록
-  const lastRun = useRef(Date.now());
+  const lastRun = useRef<number | null>(null);
 
   return (callback: () => void, delay: number) => {
-    // 이후 throttle이 트리거 되면 트리거 될 당시의 시간과 lastRun의 차이를 계산해서
-    // 그 차이가 delay 이상이라면 lastRun이 기록된 이후로 delay 만큼의 시간이 지났다는 뜻이므로 callback을 호출 함
-    const timeElapsed = Date.now() - lastRun.current;
-
-    if (timeElapsed >= delay) {
+    // 최초 실행일 경우 바로 callback 호출하고 lastRun을 현재 시간으로 업데이트
+    if (lastRun.current === null) {
       callback();
       lastRun.current = Date.now();
+    } else {
+      // 이후 throttle이 트리거 되면 트리거 될 당시의 시간과 lastRun의 차이를 계산해서
+      // 그 차이가 delay 이상이라면 lastRun이 기록된 이후로 delay 만큼의 시간이 지났다는 뜻이므로 callback을 호출 함
+      const timeElapsed = Date.now() - lastRun.current;
+
+      if (timeElapsed >= delay) {
+        callback();
+        lastRun.current = Date.now();
+      }
     }
   };
 };

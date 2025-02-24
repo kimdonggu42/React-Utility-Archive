a5458b2a834131a75f49327b513681b6eaacd1d8
kimdonggu42 <kdk3280@undermilli.com>
2025-02-24 23:57:55 +0900

    feat: createPortal 적용으로 모달 구조 개선

--- client/src/pages/ui/modal/modal-demo.tsx
+++ client/src/pages/ui/modal/modal-demo.tsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom';
 import Modal from '@/components/Modal';
 import { useToggle } from '@/hooks/useToggle';
 
@@ -14,7 +15,15 @@ export default function ModalDemo() {
           모달
         </button>
       </div>
-      {isToggled && <Modal modalToggleHandler={toggle}>모달 내용</Modal>}
+      {/* createPortal을 사용하면, React 컴포넌트 트리 바깥의 DOM 노드로 렌더링할 수 있다.
+          예를 들어, body에 직접 모달을 렌더링하면 부모 컴포넌트의 CSS 속성(z-index, overflow 등)에
+          영향을 받지 않고 독립적으로 UI를 구성할 수 있어 모달, 툴팁 등에 자주 사용된다.
+          모달이나 툴팁 같은 UI 요소는 전체 페이지를 덮는 오버레이이므로, 부모 요소의 DOM 구조에 중첩되도록 렌더링하는 것은 의미적·구조적 측면에서 적절하지 않다.
+          비록 스타일로는 동작이 가능해 보여도, HTML 구조가 복잡해지고 의미가 흐려지기 쉽다.
+          따라서 이러한 오버레이 컴포넌트는 createPortal을 사용해 document.body 같은 최상위 DOM 노드에 직접 렌더링하는 것이 좋다.
+          https://ko.react.dev/reference/react-dom/createPortal */}
+      {isToggled &&
+        createPortal(<Modal modalToggleHandler={toggle}>모달 내용</Modal>, document.body)}
     </>
   );
 }
